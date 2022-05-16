9d3d9895c6ba23e810de5b0fe2bfec03f19964d2
Saif Sabban <66663433+saifsabban@users.noreply.github.com>
2022-05-16 17:45:09 +0100

    Migration into Github

---
+++ My own Version of code to let tv work/buttons.py
@@ -0,0 +1,34 @@
+import RPi.GPIO as GPIO
+import time
+import os
+
+os.system('raspi-gpio set 19 ip')
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(26, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+def turnOnScreen():
+    os.system('raspi-gpio set 19 op a5')
+    os.system('raspi-gpio set 18 op a5')
+    os.system('vcgencmd display_power 1')
+
+
+def turnOffScreen():
+    os.system('raspi-gpio set 19 ip')
+    os.system('raspi-gpio set 18 ip')
+    os.system('vcgencmd display_power 0')
+
+turnOffScreen()
+screen_on = False
+
+while (True):
+    # If you are having and issue with the button doing the opposite of what you want
+    # IE Turns on when it should be off, change this line to:
+    # input = not GPIO.input(26)
+    input = GPIO.input(26)
+    if input != screen_on:
+        screen_on = input
+        if screen_on:
+            turnOnScreen()
+        else:
+            turnOffScreen()
+    time.sleep(0.3)
