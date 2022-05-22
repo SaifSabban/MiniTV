618fd61d66bedd325ae3ea54eb70e649bf78f99d
Saif Sabban <66663433+saifsabban@users.noreply.github.com>
2022-05-22 12:18:06 +0100

    Update buttons.py

--- ButtonsKnobCode/buttons.py
+++ ButtonsKnobCode/buttons.py
@@ -10,12 +10,13 @@ def turnOnScreen():
     os.system('raspi-gpio set 19 op a5')
     os.system('raspi-gpio set 18 op a5')
     os.system('vcgencmd display_power 1')
-
+    os.system('tvservice --sdtvon="NTSC [P]"')
 
 def turnOffScreen():
     os.system('raspi-gpio set 19 ip')
     os.system('raspi-gpio set 18 ip')
     os.system('vcgencmd display_power 0')
+    os.system('tvservice -o')
 
 turnOffScreen()
 screen_on = False
